577ab1cc51436321d6da49f8a1557c1c2cebedb3
baryTest <111561132+baryTest@users.noreply.github.com>
2022-09-07 09:11:47 +0200

    Add files via upload

---
+++ leschaines _1.py
@@ -0,0 +1,20 @@
+
+# -*- coding: utf-8 -*-
+
+
+from unittest import result
+
+ 
+
+
+age  = int( input('quel est ton age ?'))                
+hasPermis = bool( input( 'as-tu ton permis (Y/N)'))
+if result.lower() in (listeTrue):
+    hasPermis = True
+nbrpoint  = input('nombre de points?')
+
+
+
+
+print ( age*5, hasPermis, nbrpoint)
+
